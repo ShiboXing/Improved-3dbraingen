1b4aa75b6c0cf9aac02e330434fbeeea0e809033
Shibo Xing <shx26@pitt.edu>
2020-08-29 09:33:48 -0400

    feat: utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -12,10 +12,10 @@ def load_checkpoint(G, D, E, CD, fname):
             curr_num = int(s.split('iter')[1].split('.')[0])
             highest_pth = max(highest_pth, curr_num)
         if files:
-            D.load_state_dict(torch.load(f'./checkpoint/{fname}{highest_pth}.pth'))
-            CD.load_state_dict(torch.load(f'./checkpoint/{fname}{highest_pth}.pth'))
-            E.load_state_dict(torch.load(f'./checkpoint/{fname}{highest_pth}.pth'))
-            G.load_state_dict(torch.load(f'./checkpoint/{fname}{highest_pth}.pth'))
+            D.load_state_dict(torch.load(f'./checkpoint/D{fname}{highest_pth}.pth'))
+            CD.load_state_dict(torch.load(f'./checkpoint/CD{fname}{highest_pth}.pth'))
+            E.load_state_dict(torch.load(f'./checkpoint/E{fname}{highest_pth}.pth'))
+            G.load_state_dict(torch.load(f'./checkpoint/G{fname}{highest_pth}.pth'))
             iteration = highest_pth
     else:
         os.mkdir(checkpoint_pth)
